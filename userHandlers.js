1ac99fcf07a87bc8dff54d46b01ae97a88d75831
anohjr <rosenaomi.hj@gmail.com>
2023-05-22 17:00:42 +0200

    put path ok

--- userHandlers.js
+++ userHandlers.js
@@ -43,6 +43,28 @@ const postUser = (req, res) => {
         .catch((err) => {
             console.error(err);
             res.status(500).send("Error saving user");
+        })  
+}
+
+const updateUser = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { firstname, lastname, email, city, language } = req.body;
+
+    database
+        .query(
+            "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+            [firstname, lastname, email, city, language, id]
+        )
+        .then(([result]) => {
+            if (result.affectedRows === 0) {
+                res.status(404).send("Not Found");
+            } else {
+                res.sendStatus(204);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send ("Error editing user");
         })
 }
 
@@ -50,4 +72,5 @@ module.exports = {
     getUsers,
     getUserById,
     postUser,
+    updateUser,
 };
