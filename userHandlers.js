daa3af4e800c5118b914b5a0b6c734b1d57a8098
anohjr <rosenaomi.hj@gmail.com>
2023-05-22 10:52:26 +0200

    quest Express 02 finished

---
+++ userHandlers.js
@@ -0,0 +1,33 @@
+
+const database = require("./database");
+
+const getUsers = (req, res) => {
+    database
+    .query("select * from users")
+    .then(([users]) => {
+        res.json(users);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).send("Error retrieving data from database")
+    })
+};
+
+const getUserById = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    database
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+        if (users[0] != null) {
+            res.json(users[0]);
+        } else {
+            res.status(404).send("Not Found");
+        }
+    })
+}
+
+module.exports = {
+    getUsers,
+    getUserById
+};
